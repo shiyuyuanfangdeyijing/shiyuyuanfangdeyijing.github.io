30ec3d561cad69f5f1aff91dac1cc47bba807e3e
shiyuyuanfangdeyijing <117435038+shiyuyuanfangdeyijing@users.noreply.github.com>
2025-02-07 22:58:18 +0800

    newYear.js

--- js/newYear.js
+++ js/newYear.js
@@ -41,8 +41,6 @@ var newYear = () => {
     time();
 };
 
-newYearElement.style.backgroundImage = 'none';
-
 // 防止 Pjax 事件重复绑定
 document.removeEventListener('pjax:complete', newYear);
 document.addEventListener('pjax:complete', newYear);
